5d5955c97328e8cb57a67f6abb869503965eb0bf
joao.teixeira2924@gmail.com <joao.teixeira2924@gmail.com>
2022-12-01 16:51:53 +0000

    Initial changes to template

--- clientTCP.c
+++ clientTCP.c
@@ -18,10 +18,28 @@ int main(int argc, char **argv) {
 
     if (argc > 1)
         printf("**** No arguments needed. They will be ignored. Carrying ON.\n");
-    int sockfd;
-    struct sockaddr_in server_addr;
     char buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";
     size_t bytes;
+    /*send a string to the server*/
+    int sockfd = create_connect_socket();
+    bytes = write(sockfd, buf, strlen(buf));
+    if (bytes > 0)
+        printf("Bytes escritos %ld\n", bytes);
+    else {
+        perror("write()");
+        exit(-1);
+    }
+
+    if (close(sockfd)<0) {
+        perror("close()");
+        exit(-1);
+    }
+    return 0;
+}
+
+int create_connect_socket() {
+    int sockfd;
+    struct sockaddr_in server_addr;
 
     /*server address handling*/
     bzero((char *) &server_addr, sizeof(server_addr));
@@ -41,20 +59,5 @@ int main(int argc, char **argv) {
         perror("connect()");
         exit(-1);
     }
-    /*send a string to the server*/
-    bytes = write(sockfd, buf, strlen(buf));
-    if (bytes > 0)
-        printf("Bytes escritos %ld\n", bytes);
-    else {
-        perror("write()");
-        exit(-1);
-    }
-
-    if (close(sockfd)<0) {
-        perror("close()");
-        exit(-1);
-    }
-    return 0;
+    return sockfd;
 }
-
-
